659b9d5766943e667243cba1531f9d0b63bc674d
JordyDevel <graciamancilla@outlook.com>
2018-12-14 00:31:11 -0600

    Commit inicial, propuesta para instalar en el PIC Maestro

---
+++ MasterPIC.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   MasterPIC.c
+ * Author: Gracia Mancilla Jordy Jesus
+ *
+ * Created on 14 de diciembre de 2018, 12:22 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
