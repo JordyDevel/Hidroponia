c4e08cffb5ff7bd7f192155396b6aa78ad4a03d5
JordyDevel <graciamancilla@outlook.com>
2018-12-15 21:38:43 -0600

    Sincronización de repositorio Remoto y Local

--- MasterPIC.c
+++ MasterPIC.c
@@ -8,11 +8,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
 
-    return (EXIT_SUCCESS);
+void main(void) {
+    while(true) {
+        // this is an infinite loop
+    }
 }
-
